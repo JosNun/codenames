e9c0f6a7094d37556c768ab5d1f626d53395498f
Josiah Nunemaker <yoshiya.d.n@gmail.com>
2018-02-21 14:00:13 -0500

    theme changes

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 /*
 TODO:
-[ ] Remove the extra game names from the datalist OR
+[X] Remove the extra game names from the datalist OR
 [ ] Overhaul game selection
 [ ] Bolden the players name, and indicate what team they are on
 [X] Abort card guess if it is not that team's turn
 [ ] check player socket id before joining game
-[ ] Maybe save the socket id using the web storage API, then, use that for reconnecting
+[ ] Maybe save the socket id using the web storage API, then,
+use that for reconnecting
+[ ] Allow a way to set a player's name in the menu
+[ ] Show game info in setup screen
+[ ] Mobilify it
 [ ] Clean up everything, and reorder it
 */
 
